eadafe671a32f673c86cd1283f4c69333142f628
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2020-06-21 18:35:14 +0200

    to do list in portfolio

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+// Create a "close" button and append it to each list item
+
+var myNodelist = document.getElementsByTagName("li");
+var i;
+for (i = 0; i < myNodelist.length; i++) 
+{
+    var span = document.createElement("span");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+
+var close = document.getElementsByTagName("close");
+var i;
+for (i = 0; i < close.length; i++)
+{
+    close[i].onclick = function()
+    {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+    
+}
+
+//
