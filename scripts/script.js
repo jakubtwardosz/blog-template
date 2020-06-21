986e9d8e7f27c15bc52808a6bc63a63331b6505c
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2020-06-21 18:43:35 +0200

    js to do list

--- scripts/script.js
+++ scripts/script.js
@@ -25,4 +25,13 @@ for (i = 0; i < close.length; i++)
     
 }
 
-//
+// Add a "checked" symbol when clicking on a list item
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev)
+{
+   if (ev.target.tagName === 'li')
+   {
+       ev.target.classList.toggle('checked');
+   } 
+}, false );
