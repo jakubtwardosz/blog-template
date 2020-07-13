7567bc42d627f695e596f83ed0e7cd2905fd1352
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2020-07-13 23:58:24 +0200

    Next todo - mark a task as completed

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,25 @@
 let toDoItems = [];
 
-function addToDo(text)  {
+function renderToDo(toDo) {
+    const list = document.querySelector('.js-todo-list');
+
+    const isChecked = toDo.checked ? 'done' : '';
+    const node = document.createElement("li");
+    node.setAttribute('class', 'todo-item ${isChecked}');
+    node.setAttribute('data-key', toDo.id);
+    node.innerHTML = `
+        <input id="${toDo.id}" type="checkbox"/>
+        <label for="${toDo.id}" class="tick js-tick"></label>
+        <span>${toDo.text}</span>
+        <button class="delete-todo js-delete-todo" href="#delete-icon">
+        <img src="./assets/svg/circle-x.svg" alt="Delete"/>        
+        </button>
+    `;
+
+    list.append(node);
+}
+
+function addToDo(text) {
     const toDo = {
         text,
         checked: false,
@@ -8,22 +27,21 @@ function addToDo(text)  {
     };
 
     toDoItems.push(toDo);
-    console.log(toDoItems);
+    renderToDo(toDo);
 }
 
 const form = document.querySelector('.js-form');
-
 form.addEventListener('submit', event => {
-
     event.preventDefault();
-
     const input = document.querySelector('.js-todo-input');
 
     const text = input.value.trim();
-
     if (text !== ''){
         addToDo(text);
         input.value = '';
         input.focus();
     }
 });
+
+//next to do Mark a task as completed
+
