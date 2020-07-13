7be987fc60bf4a9185cd3754324967308d186ae7
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2020-07-13 20:28:24 +0200

    first steps with todolist

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,29 @@
-// Create a "close" button and append it to each list item
+let toDoItems = [];
 
-var myNodelist = document.getElementsByTagName("li");
-var i;
-for (i = 0; i < myNodelist.length; i++) 
-{
-    var span = document.createElement("span");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
+function addToDo(text)  {
+    const toDo = {
+        text,
+        checked: false,
+        id: Date.now()
+    };
+
+    toDoItems.push(toDo);
+    console.log(toDoItems);
 }
 
-// Click on a close button to hide the current list item
+const form = document.querySelector('.js-form');
 
-var close = document.getElementsByTagName("close");
-var i;
-for (i = 0; i < close.length; i++)
-{
-    close[i].onclick = function()
-    {
-        var div = this.parentElement;
-        div.style.display = "none";
+form.addEventListener('submit', event => {
+
+    event.preventDefault();
+
+    const input = document.querySelector('.js-todo-input');
+
+    const text = input.value.trim();
+
+    if (text !== ''){
+        addToDo(text);
+        input.value = '';
+        input.focus();
     }
-    
-}
-
-// Add a "checked" symbol when clicking on a list item
-
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev)
-{
-   if (ev.target.tagName === 'li')
-   {
-       ev.target.classList.toggle('checked');
-   } 
-}, false );
+});
